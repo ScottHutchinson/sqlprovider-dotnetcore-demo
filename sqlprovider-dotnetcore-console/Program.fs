e8087bc82082c94963b885504a3abdc9ae52fc0f
Scott Hutchinson <scotthutchinson@usa.net>
2018-03-25 10:51:25 -0700

    Minimal, Complete, and Verifiable example

--- sqlprovider-dotnetcore-console/Program.fs
+++ sqlprovider-dotnetcore-console/Program.fs
@@ -1,23 +1,15 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
-open System.IO
-open Microsoft.Extensions.Configuration
 open FSharp.Data.Sql
 
-let builder = 
-    let ret = new ConfigurationBuilder()
-    FileConfigurationExtensions.SetBasePath(ret, Directory.GetCurrentDirectory()) |> ignore
-    JsonConfigurationExtensions.AddJsonFile(ret, "appSettings.json")
-
-let config = builder.Build ()
 [<Literal>]
 let ConnStr = "Data Source=localhost; Initial Catalog=AdventureWorks2017; Integrated Security=True"
 type HR = SqlDataProvider<Common.DatabaseProviderTypes.MSSQLSERVER, ConnStr>
 
 [<EntryPoint>]
 let main argv =
-    let runtimeConnectionString = config.Item("App:Connection:Value")
+    let runtimeConnectionString = ConnStr
     let ctx = HR.GetDataContext runtimeConnectionString
     let employeeJobTitle = 
         query {
