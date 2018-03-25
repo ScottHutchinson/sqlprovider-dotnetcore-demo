a44d891b97a576666264ad24c5da0010da45008e
Scott Hutchinson <scotthutchinson@usa.net>
2018-03-25 10:29:41 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# SQLProvider Example F# dotnet core project
+
+This sample console application demonstrates how to use the SQLProvider type provider to query a Microsoft SQL Server database. It queries the AdventureWorks2017 database, which you can download at https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorks2017.bak
+
+This sample also demonstrates how to use a json config file with an F# dotnetcore project.
+
+
+## Articles Related to SQLProvider
+
+
---
+++ sqlprovider-dotnetcore-console/Program.fs
@@ -0,0 +1,30 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.IO
+open Microsoft.Extensions.Configuration
+open FSharp.Data.Sql
+
+let builder = 
+    let ret = new ConfigurationBuilder()
+    FileConfigurationExtensions.SetBasePath(ret, Directory.GetCurrentDirectory()) |> ignore
+    JsonConfigurationExtensions.AddJsonFile(ret, "appSettings.json")
+
+let config = builder.Build ()
+[<Literal>]
+let ConnStr = "Data Source=localhost; Initial Catalog=AdventureWorks2017; Integrated Security=True"
+type HR = SqlDataProvider<Common.DatabaseProviderTypes.MSSQLSERVER, ConnStr>
+
+[<EntryPoint>]
+let main argv =
+    let runtimeConnectionString = config.Item("App:Connection:Value")
+    let ctx = HR.GetDataContext runtimeConnectionString
+    let employeeJobTitle = 
+        query {
+            for emp in ctx.HumanResources.Employee do
+            select emp.JobTitle
+        } |> Seq.head
+
+    printfn "Hello %s!" employeeJobTitle
+    System.Threading.Thread.Sleep 2000
+    0 // return an integer exit code
